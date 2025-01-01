5d225a5ab8608cdaf5c90b7fe4feded7736a1146
William Cahill <drewlikesramen@gmail.com>
2025-01-01 15:17:32 -0500

    Got basic crud operations working

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize)]
 pub struct Graph {
-   todos: Slab<Todo>,
+   tasks: Slab<Task>,
 }
 
 impl Graph {
@@ -13,168 +13,173 @@ impl Graph {
         Self::default()
     }
 
-    pub fn insert(&mut self, todo: Todo) -> TodoId {
-        self.todos.insert(todo)
+    pub fn insert(&mut self, task: Task) -> TaskId {
+        self.tasks.insert(task)
     }
-    
-    pub fn insert_with_dependencies<D>(&mut self, mut todo: Todo, dependencies: D) -> Result<TodoId>
+
+    pub fn insert_with_children<D>(&mut self, mut task: Task, children: D) -> Result<TaskId>
     where 
-        D: Into<Vec<TodoId>>,
+        D: Into<Vec<TaskId>>,
     {
-        let dependencies = dependencies.into();
-        for dep_id in &dependencies {
-            if !self.contains_todo(*dep_id) { return Err(GraphError::TodoNotFound) }
+        let children = children.into();
+        for child_id in &children {
+            if !self.contains_task(*child_id) { return Err(GraphError::TaskNotFound) }
         }
-        todo.dependencies = dependencies;
-        Ok(self.todos.insert(todo))
+        task.children = children;
+        Ok(self.tasks.insert(task))
     }
 
-    pub fn remove(&mut self, todo_id: TodoId) -> Option<Todo> {
-        let mut todo = self.todos.try_remove(todo_id)?;
-        todo.dependencies.clear();
-        for (_, t) in &mut self.todos {
-           t.dependencies.retain(|tid| *tid != todo_id); 
+    pub fn remove(&mut self, task_id: TaskId) -> Option<Task> {
+        let mut task = self.tasks.try_remove(task_id)?;
+        task.children.clear();
+        for (_, t) in &mut self.tasks {
+           t.children.retain(|tid| *tid != task_id); 
         }
-        Some(todo) 
+        Some(task) 
     }
 
-    pub fn get(&self, todo_id: TodoId) -> Option<&Todo> {
-        self.todos.get(todo_id)
+    pub fn get(&self, task_id: TaskId) -> Option<&Task> {
+        self.tasks.get(task_id)
     }
 
-    pub fn get_mut(&mut self, todo_id: TodoId) -> Option<&mut Todo> {
-        self.todos.get_mut(todo_id)
+    pub fn get_mut(&mut self, task_id: TaskId) -> Option<&mut Task> {
+        self.tasks.get_mut(task_id)
     }
 
-    pub fn iter(&self) -> impl Iterator<Item = (TodoId, &Todo)> {
-        self.todos.iter()
+    pub fn iter(&self) -> impl Iterator<Item = (TaskId, &Task)> {
+        self.tasks.iter()
     }
 
-    pub fn iter_mut(&mut self) -> impl Iterator<Item = (TodoId, &mut Todo)> {
-        self.todos.iter_mut()
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (TaskId, &mut Task)> {
+        self.tasks.iter_mut()
     }
 
     pub fn len(&self) -> usize {
-        self.todos.len()
+        self.tasks.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.todos.is_empty()
+        self.tasks.is_empty()
     }
 
-    pub fn insert_dependency(&mut self, todo_id: TodoId, dep_id: TodoId) -> Result<()> {
-        if !self.contains_todo(dep_id) { return Err(GraphError::TodoNotFound) }
-        let todo = self.todos.get_mut(todo_id).ok_or(GraphError::TodoNotFound)?;
-        if !todo.dependencies.iter().any(|id| *id == dep_id) {
-            todo.dependencies.push(dep_id);
+    pub fn insert_child(&mut self, task_id: TaskId, child_id: TaskId) -> Result<()> {
+        if !self.contains_task(child_id) { return Err(GraphError::TaskNotFound) }
+        let task = self.tasks.get_mut(task_id).ok_or(GraphError::TaskNotFound)?;
+        if !task.children.iter().any(|id| *id == child_id) {
+            task.children.push(child_id);
         }
         Ok(()) 
     }
 
-    pub fn remove_dependency(&mut self, todo_id: TodoId, dep_id: TodoId) -> Result<()> {
-        let todo = self.todos.get_mut(todo_id).ok_or(GraphError::TodoNotFound)?;
-        todo.dependencies.retain(|id| *id != dep_id);
+    pub fn remove_child(&mut self, task_id: TaskId, child_id: TaskId) -> Result<()> {
+        let task = self.tasks.get_mut(task_id).ok_or(GraphError::TaskNotFound)?;
+        task.children.retain(|id| *id != child_id);
         Ok(()) 
     }
 
-    fn contains_todo(&self, todo_id: TodoId) -> bool {
-        self.todos.iter().any(|(tid, _)| tid == todo_id)
+    fn contains_task(&self, task_id: TaskId) -> bool {
+        self.tasks.iter().any(|(tid, _)| tid == task_id)
     }
 }
 
 #[derive(Error, Debug)]
 pub enum GraphError {
-    #[error("Todo not found")]
-    TodoNotFound,
+    #[error("task not found")]
+    TaskNotFound,
 }
 
 pub type Result<T> = std::result::Result<T, GraphError>;
 
 #[derive(Serialize, Deserialize, Clone, Eq, PartialEq, Default, Debug)]
-pub struct Todo { 
+pub struct Task { 
     pub name: String,
-    dependencies: Vec<TodoId>,
+    #[serde(rename="children")]
+    children: Vec<TaskId>,
 }
 
-impl Todo {
+impl Task {
     pub fn new(name: impl Into<String>) -> Self {
         Self {
             name: name.into(),
-            dependencies: vec![],
+            children: vec![],
         }
+    }
+
+    pub fn children(&self) -> &[TaskId] {
+        &self.children
     }
 }
 
-pub type TodoId = usize;
+pub type TaskId = usize;
 
 
 #[cfg(test)]
 mod test {
-    use crate::{ Graph, Todo };
+    use crate::{ Graph, Task };
 
     #[test]
     fn test_insertion_and_retrieval() {
         let mut graph = Graph::new();
-        let find_keys_id = graph.insert(Todo::new("Find Keys"));
-        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys_id = graph.insert(Task::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Task::new("Find Wallet"));
         let find_keys = graph.get(find_keys_id).unwrap();
         let find_wallet = graph.get(find_wallet_id).unwrap();
-        assert_eq!(find_keys, &Todo::new("Find Keys"));
-        assert_eq!(find_wallet, &Todo::new("Find Wallet"));
+        assert_eq!(find_keys, &Task::new("Find Keys"));
+        assert_eq!(find_wallet, &Task::new("Find Wallet"));
     }
 
     #[test]
     fn test_removal() {
         let mut graph = Graph::new();
-        let find_keys_id = graph.insert(Todo::new("Find Keys"));
-        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys_id = graph.insert(Task::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Task::new("Find Wallet"));
         let find_keys = graph.remove(find_keys_id);
         let find_wallet = graph.get(find_wallet_id);
         let find_keys_ref = graph.get(find_keys_id);
-        assert_eq!(find_keys, Some(Todo::new("Find Keys")));
-        assert_eq!(find_wallet, Some(&Todo::new("Find Wallet")));
+        assert_eq!(find_keys, Some(Task::new("Find Keys")));
+        assert_eq!(find_wallet, Some(&Task::new("Find Wallet")));
         assert_eq!(find_keys_ref, None); 
     }
 
     #[test]
-    fn test_insert_with_dependencies() {
+    fn test_insert_with_children() {
         let mut graph = Graph::new();
-        let find_keys_id = graph.insert(Todo::new("Find Keys"));
-        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys_id = graph.insert(Task::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Task::new("Find Wallet"));
         let get_groceries_id = graph
-            .insert_with_dependencies(Todo::new("Get Groceries"), [find_keys_id, find_wallet_id])
+            .insert_with_children(Task::new("Get Groceries"), [find_keys_id, find_wallet_id])
             .unwrap();
         let get_groceries = graph.get(get_groceries_id).unwrap();
         assert_eq!(get_groceries.name.as_str(), "Get Groceries");
-        assert_eq!(get_groceries.dependencies, &[find_keys_id, find_wallet_id]);
+        assert_eq!(get_groceries.children, &[find_keys_id, find_wallet_id]);
     }
 
     #[test]
-    fn test_insert_dependency() {
+    fn test_insert_child() {
         let mut graph = Graph::new();
-        let find_keys_id        = graph.insert(Todo::new("Find Keys"));
-        let find_wallet_id      = graph.insert(Todo::new("Find Wallet"));
-        let get_groceries_id    = graph.insert(Todo::new("Get Groceries"));
-        graph.insert_dependency(get_groceries_id, find_keys_id).unwrap();
-        graph.insert_dependency(get_groceries_id, find_wallet_id).unwrap();
+        let find_keys_id        = graph.insert(Task::new("Find Keys"));
+        let find_wallet_id      = graph.insert(Task::new("Find Wallet"));
+        let get_groceries_id    = graph.insert(Task::new("Get Groceries"));
+        graph.insert_child(get_groceries_id, find_keys_id).unwrap();
+        graph.insert_child(get_groceries_id, find_wallet_id).unwrap();
         let get_groceries = graph.get(get_groceries_id).unwrap();
         assert_eq!(get_groceries.name.as_str(), "Get Groceries");
-        assert_eq!(get_groceries.dependencies, &[find_keys_id, find_wallet_id]);
+        assert_eq!(get_groceries.children, &[find_keys_id, find_wallet_id]);
     }
 
     #[test]
-    fn test_removal_with_dependencies() {
+    fn test_removal_with_children() {
         let mut graph = Graph::new();
-        let find_keys_id = graph.insert(Todo::new("Find Keys"));
-        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys_id = graph.insert(Task::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Task::new("Find Wallet"));
         let get_groceries_id = graph
-            .insert_with_dependencies(Todo::new("Get Groceries"), [find_keys_id, find_wallet_id])
+            .insert_with_children(Task::new("Get Groceries"), [find_keys_id, find_wallet_id])
             .unwrap();
         graph.remove(find_keys_id);
         let get_groceries = graph.get(get_groceries_id).unwrap();
-        assert_eq!(get_groceries.dependencies, &[find_wallet_id]);
+        assert_eq!(get_groceries.children, &[find_wallet_id]);
         graph.remove(find_wallet_id);
         let get_groceries = graph.get(get_groceries_id).unwrap();
-        assert_eq!(get_groceries.dependencies, &[]);
+        assert_eq!(get_groceries.children, &[]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,123 @@
+use std::fmt;
 use std::{env, fs};
 use std::path::{PathBuf, Path};
+use clap::{command, Parser, Subcommand};
 use thiserror::Error;
-use todochad::Graph;
+use tabled::{Table, Tabled};
+use tod::{Graph, GraphError, Task, TaskId};
 
-const APP_NAME: &str        = "todochad";
+const APP_NAME: &str        = "tod";
 const GRAPH_FILE_NAME: &str = "graph.ron";
 
-fn main() -> Result<()> {
+#[derive(Parser, Debug)]
+#[command(name="tod")]
+#[command(version="0.1")]
+#[command(about="A todo list generator using a dependency graph")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,  
+}
+
+#[derive(Subcommand, Debug)]
+enum Command { 
+    #[command(name="add", about="Add a task")]
+    Add { 
+        task_name: String,
+    },
+    #[command(name="rm", about="Remove a task")]
+    Remove { 
+        task_id: TaskId,
+    }, 
+    #[command(name="ls", about="List tasks with table decorations")]
+    List {
+        #[clap(long, short, help="Strips away table decorations")]
+        simple: bool,
+    },
+    #[command(name="clear", about="Clear all tasks")]
+    Clear,
+    #[command(name="depadd", about="Adds child tasks as dependencies of another task")]
+    DepAdd {
+        #[clap(help="Task that will receive child tasks")]
+        task_id: TaskId,
+        #[clap(required=true, help="Child task(s) to add")]
+        child_ids: Vec<TaskId>,
+    },
+    #[command(name="deprm", about="Removes child tasks as dependencies from another task")]
+    DepRemove {
+        #[clap(help="Task that will remove child tasks")]
+        task_id: TaskId,
+        #[clap(required=true, help="Child task(s) to remove")]
+        child_ids: Vec<TaskId>,
+    },
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {err}");
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let cli = Cli::parse();
+    run_command(cli.command)?;
+    Ok(())
+}
+
+/// Runs a command on a graph.
+/// Returns true if the graph was edited.
+fn run_command(command: Command) -> Result<()> {
     let graph_path = graph_path()?;
-    let mut graph = load_graph(&graph_path)?;
-    save_graph(&graph_path, &graph)?;
+    match command {
+        Command::Add { task_name } => {
+            let mut graph = load_graph(&graph_path)?;
+            let task_id = graph.insert(Task::new(task_name));
+            save_graph(&graph_path, &graph)?;
+            println!("{task_id}");
+        },
+        Command::Remove { task_id } => {
+            let mut graph = load_graph(&graph_path)?;
+            graph.remove(task_id).ok_or(GraphError::TaskNotFound)?;
+            save_graph(&graph_path, &graph)?;
+        },
+        Command::List { simple } => {
+            let graph = load_graph(&graph_path)?;
+            if !simple {
+                let task_rows = graph.iter().map(|(task_id, task)| TaskRow::new(task_id, task));
+                let task_table = Table::new(task_rows);
+                println!("{task_table}");
+            }
+            else {
+                for (task_id, task) in graph.iter() {
+                    let task_row = TaskRow::new(task_id, task);
+                    task_row.print();
+                }
+            }
+        },
+        Command::Clear => {
+            match fs::exists(&graph_path) { 
+                Err(_) => return Err(AppError::GraphReadError),
+                Ok(false) => {},
+                Ok(true) => {
+                    std::fs::remove_file(graph_path).map_err(|_| AppError::GraphDeleteError)?;
+                },
+            }
+        },
+        Command::DepAdd { task_id, child_ids } => {
+            let mut graph = load_graph(&graph_path)?;
+            for child_id in child_ids {
+                graph.insert_child(task_id, child_id)?;
+            }
+            save_graph(&graph_path, &graph)?;
+        },
+        Command::DepRemove { task_id, child_ids } => {
+            let mut graph = load_graph(&graph_path)?;
+            for child_id in child_ids {
+                graph.remove_child(task_id, child_id)?;
+            }
+            save_graph(&graph_path, &graph)?;
+        },
+    }
     Ok(())
 }
 
@@ -18,8 +126,8 @@ fn load_graph(graph_path: &Path) -> Result<Graph> {
         Err(_) => Err(AppError::GraphReadError),
         Ok(false) => Ok(Graph::new()),
         Ok(true) => {
-            let graph_str = fs::read_to_string(graph_path).or(Err(AppError::GraphReadError))?;
-            let graph = ron::from_str(&graph_str).or(Err(AppError::GraphParseError))?;
+            let graph_str = fs::read_to_string(graph_path).map_err(|_| AppError::GraphReadError)?;
+            let graph = ron::from_str(&graph_str).map_err(|_| AppError::GraphParseError)?;
             Ok(graph)
         },
     }
@@ -27,14 +135,14 @@ fn load_graph(graph_path: &Path) -> Result<Graph> {
 
 fn save_graph(graph_path: &Path, graph: &Graph) -> Result<()> {
     let graph_str = ron::to_string(graph).expect("Failed to serialize graph");
-    fs::write(graph_path, graph_str).or(Err(AppError::GraphWriteError))?;
+    fs::write(graph_path, graph_str).map_err(|_| AppError::GraphWriteError)?;
     Ok(())
 }
 
 /// Determines path of graph file.
 /// Creates directory structure along the way if it does not exist.
 fn graph_path() -> Result<PathBuf> {
-    let home = env::var("HOME").or(Err(AppError::HomeDirError))?;
+    let home = env::var("HOME").map_err(|_| AppError::HomeDirError)?;
     let graph_path = format!("{home}/.local/share/{APP_NAME}/{GRAPH_FILE_NAME}");
     let graph_path = PathBuf::from(graph_path);
     if let Some(graph_dir) = graph_path.parent() {
@@ -44,6 +152,46 @@ fn graph_path() -> Result<PathBuf> {
         }
     }
     Ok(graph_path)
+}
+
+/// Printable task record
+#[derive(Tabled)]
+struct TaskRow<'a> {
+    id: TaskId,
+    name: &'a str,
+    dependencies: Dependencies<'a>, 
+}
+
+impl<'a> TaskRow<'a> {
+    fn new(id: TaskId, task: &'a Task) -> Self {
+        Self {
+            id,
+            name: &task.name,
+            dependencies: Dependencies(task.children()),
+        }
+    }
+
+    fn print(&self) {
+        println!("{} \"{}\" {}", self.id, self.name, self.dependencies);
+    }
+}
+
+/// Printable list of a task's dependencies
+struct Dependencies<'a>(&'a [TaskId]);
+impl fmt::Display for Dependencies<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.0.len() {
+            1 => write!(f, "{}", self.0[0])?,
+            2.. => {
+                write!(f, "{}", self.0[0])?;
+                for task_id in &self.0[1..] {
+                    write!(f, ",{task_id}")?;
+                }
+            },
+            _ => {},
+        }
+        Ok(())
+    }
 }
 
 
@@ -57,6 +205,10 @@ pub enum AppError {
     GraphParseError,
     #[error("Failed to write graph file")]
     GraphWriteError,
+    #[error("Failed delete graph file")]
+    GraphDeleteError,
+    #[error(transparent)]
+    GraphError(#[from] GraphError),
 }
 
 type Result<T> = std::result::Result<T, AppError>;
