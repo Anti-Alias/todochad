0ba477d3a1b1dbb144366013dee0c221f737fc4d
William Cahill <drewlikesramen@gmail.com>
2024-12-31 20:27:08 -0500

    Created main function scaffolding

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 use slab::Slab;
-use std::collections::HashMap;
 use thiserror::*;
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Default, Debug, Serialize, Deserialize)]
 pub struct Graph {
    todos: Slab<Todo>,
-   dependencies: HashMap<TodoId, Vec<TodoId>>,
 }
 
 impl Graph {
@@ -18,10 +16,25 @@ impl Graph {
     pub fn insert(&mut self, todo: Todo) -> TodoId {
         self.todos.insert(todo)
     }
+    
+    pub fn insert_with_dependencies<D>(&mut self, mut todo: Todo, dependencies: D) -> Result<TodoId>
+    where 
+        D: Into<Vec<TodoId>>,
+    {
+        let dependencies = dependencies.into();
+        for dep_id in &dependencies {
+            if !self.contains_todo(*dep_id) { return Err(GraphError::TodoNotFound) }
+        }
+        todo.dependencies = dependencies;
+        Ok(self.todos.insert(todo))
+    }
 
     pub fn remove(&mut self, todo_id: TodoId) -> Option<Todo> {
-        let todo = self.todos.try_remove(todo_id)?;
-        self.dependencies.retain(|parent_id, child_id| todo_id != *parent_id && todo_id != *child_id);
+        let mut todo = self.todos.try_remove(todo_id)?;
+        todo.dependencies.clear();
+        for (_, t) in &mut self.todos {
+           t.dependencies.retain(|tid| *tid != todo_id); 
+        }
         Some(todo) 
     }
 
@@ -41,25 +54,26 @@ impl Graph {
         self.todos.iter_mut()
     }
 
-    pub fn traverse(&self) -> Vec<(TodoId, Todo)> {
-        let mut result = vec![];
-        result
+    pub fn len(&self) -> usize {
+        self.todos.len()
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.todos.is_empty()
+    }
 
-    pub fn insert_dependency(&mut self, parent_id: TodoId, child_id: TodoId) -> Result<()> {
-        if !self.contains_todo(parent_id) || !self.contains_todo(child_id) {
-            return Err(GraphError::TodoNotFound);
+    pub fn insert_dependency(&mut self, todo_id: TodoId, dep_id: TodoId) -> Result<()> {
+        if !self.contains_todo(dep_id) { return Err(GraphError::TodoNotFound) }
+        let todo = self.todos.get_mut(todo_id).ok_or(GraphError::TodoNotFound)?;
+        if !todo.dependencies.iter().any(|id| *id == dep_id) {
+            todo.dependencies.push(dep_id);
         }
-        self.dependencies.insert(parent_id, child_id);
         Ok(()) 
     }
 
-    pub fn remove_dependency(&mut self, parent_id: TodoId, child_id: TodoId) -> Result<()> {
-        if !self.contains_todo(parent_id) || !self.contains_todo(child_id) {
-            return Err(GraphError::TodoNotFound);
-        }
-        self.dependencies.insert(parent_id, child_id);
+    pub fn remove_dependency(&mut self, todo_id: TodoId, dep_id: TodoId) -> Result<()> {
+        let todo = self.todos.get_mut(todo_id).ok_or(GraphError::TodoNotFound)?;
+        todo.dependencies.retain(|id| *id != dep_id);
         Ok(()) 
     }
 
@@ -76,6 +90,91 @@ pub enum GraphError {
 
 pub type Result<T> = std::result::Result<T, GraphError>;
 
-#[derive(Serialize, Deserialize, Clone, Default, Debug)]
-pub struct Todo { pub name: String }
+#[derive(Serialize, Deserialize, Clone, Eq, PartialEq, Default, Debug)]
+pub struct Todo { 
+    pub name: String,
+    dependencies: Vec<TodoId>,
+}
+
+impl Todo {
+    pub fn new(name: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            dependencies: vec![],
+        }
+    }
+}
+
 pub type TodoId = usize;
+
+
+#[cfg(test)]
+mod test {
+    use crate::{ Graph, Todo };
+
+    #[test]
+    fn test_insertion_and_retrieval() {
+        let mut graph = Graph::new();
+        let find_keys_id = graph.insert(Todo::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys = graph.get(find_keys_id).unwrap();
+        let find_wallet = graph.get(find_wallet_id).unwrap();
+        assert_eq!(find_keys, &Todo::new("Find Keys"));
+        assert_eq!(find_wallet, &Todo::new("Find Wallet"));
+    }
+
+    #[test]
+    fn test_removal() {
+        let mut graph = Graph::new();
+        let find_keys_id = graph.insert(Todo::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let find_keys = graph.remove(find_keys_id);
+        let find_wallet = graph.get(find_wallet_id);
+        let find_keys_ref = graph.get(find_keys_id);
+        assert_eq!(find_keys, Some(Todo::new("Find Keys")));
+        assert_eq!(find_wallet, Some(&Todo::new("Find Wallet")));
+        assert_eq!(find_keys_ref, None); 
+    }
+
+    #[test]
+    fn test_insert_with_dependencies() {
+        let mut graph = Graph::new();
+        let find_keys_id = graph.insert(Todo::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let get_groceries_id = graph
+            .insert_with_dependencies(Todo::new("Get Groceries"), [find_keys_id, find_wallet_id])
+            .unwrap();
+        let get_groceries = graph.get(get_groceries_id).unwrap();
+        assert_eq!(get_groceries.name.as_str(), "Get Groceries");
+        assert_eq!(get_groceries.dependencies, &[find_keys_id, find_wallet_id]);
+    }
+
+    #[test]
+    fn test_insert_dependency() {
+        let mut graph = Graph::new();
+        let find_keys_id        = graph.insert(Todo::new("Find Keys"));
+        let find_wallet_id      = graph.insert(Todo::new("Find Wallet"));
+        let get_groceries_id    = graph.insert(Todo::new("Get Groceries"));
+        graph.insert_dependency(get_groceries_id, find_keys_id).unwrap();
+        graph.insert_dependency(get_groceries_id, find_wallet_id).unwrap();
+        let get_groceries = graph.get(get_groceries_id).unwrap();
+        assert_eq!(get_groceries.name.as_str(), "Get Groceries");
+        assert_eq!(get_groceries.dependencies, &[find_keys_id, find_wallet_id]);
+    }
+
+    #[test]
+    fn test_removal_with_dependencies() {
+        let mut graph = Graph::new();
+        let find_keys_id = graph.insert(Todo::new("Find Keys"));
+        let find_wallet_id  = graph.insert(Todo::new("Find Wallet"));
+        let get_groceries_id = graph
+            .insert_with_dependencies(Todo::new("Get Groceries"), [find_keys_id, find_wallet_id])
+            .unwrap();
+        graph.remove(find_keys_id);
+        let get_groceries = graph.get(get_groceries_id).unwrap();
+        assert_eq!(get_groceries.dependencies, &[find_wallet_id]);
+        graph.remove(find_wallet_id);
+        let get_groceries = graph.get(get_groceries_id).unwrap();
+        assert_eq!(get_groceries.dependencies, &[]);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+use std::path::{PathBuf, Path};
+use thiserror::Error;
+use todochad::Graph;
+
+const APP_NAME: &str        = "todochad";
+const GRAPH_FILE_NAME: &str = "graph.ron";
+
+fn main() -> Result<()> {
+    let graph_path = graph_path()?;
+    let mut graph = load_graph(&graph_path)?;
+    save_graph(&graph_path, &graph)?;
+    Ok(())
 }
+
+fn load_graph(graph_path: &Path) -> Result<Graph> {
+    match fs::exists(graph_path) { 
+        Err(_) => Err(AppError::GraphReadError),
+        Ok(false) => Ok(Graph::new()),
+        Ok(true) => {
+            let graph_str = fs::read_to_string(graph_path).or(Err(AppError::GraphReadError))?;
+            let graph = ron::from_str(&graph_str).or(Err(AppError::GraphParseError))?;
+            Ok(graph)
+        },
+    }
+}
+
+fn save_graph(graph_path: &Path, graph: &Graph) -> Result<()> {
+    let graph_str = ron::to_string(graph).expect("Failed to serialize graph");
+    fs::write(graph_path, graph_str).or(Err(AppError::GraphWriteError))?;
+    Ok(())
+}
+
+/// Determines path of graph file.
+/// Creates directory structure along the way if it does not exist.
+fn graph_path() -> Result<PathBuf> {
+    let home = env::var("HOME").or(Err(AppError::HomeDirError))?;
+    let graph_path = format!("{home}/.local/share/{APP_NAME}/{GRAPH_FILE_NAME}");
+    let graph_path = PathBuf::from(graph_path);
+    if let Some(graph_dir) = graph_path.parent() {
+        let res = fs::create_dir_all(graph_dir);
+        if res.is_err() {
+            return Err(AppError::HomeDirError);
+        }
+    }
+    Ok(graph_path)
+}
+
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("Failed to get home directory")]
+    HomeDirError,
+    #[error("Failed to read graph file")]
+    GraphReadError,
+    #[error("Failed to parse graph file")]
+    GraphParseError,
+    #[error("Failed to write graph file")]
+    GraphWriteError,
+}
+
+type Result<T> = std::result::Result<T, AppError>;
